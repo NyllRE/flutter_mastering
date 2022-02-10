4175426354f1ec822c6e4b975238e45cfdcfd604
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-10 20:39:30 +0300

    added arrowFunction

--- dart_tutorial/bin/dart_tutorial.dart
+++ dart_tutorial/bin/dart_tutorial.dart
@@ -31,3 +31,10 @@ int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+void arrowFunction() {
+  var flybyObjects = ["sus", "susturn", "bruh"];
+
+  // todo: A shorthand => (arrow) syntax is handy for functions that contain a single statement. This syntax is especially useful when passing anonymous functions as arguments:
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
+}
