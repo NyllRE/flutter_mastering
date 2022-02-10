5e88b28ddc48fa1595f2d8ebaf5dcb803ece23f0
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-10 20:39:06 +0300

    removed junk from printInteger

--- dart_tutorial/bin/dart_tutorial.dart
+++ dart_tutorial/bin/dart_tutorial.dart
@@ -2,9 +2,9 @@
 //   print('Hello world!');
 // }
 
-// todo void : A special type that indicates a value that’s never used. Functions like printInteger() and main() that don’t explicitly return a value have the void return type
+// todo: void : A special type that indicates a value that’s never used. Functions like printInteger() and main() that don’t explicitly return a value have the void return type
 
-// todo main(): The special, required, top-level function where app execution starts.
+// todo: main(): The special, required, top-level function where app execution starts.
 
 void main() {
   var number = 1998; // ? Declare and initialize a variable.
@@ -13,26 +13,18 @@ void main() {
 }
 
 void printInteger(int year) {
-  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-  if (year >= 2001) {
-    print('21st century');
-  } else if (year >= 1901) {
-    print('20th century');
+  var value = "asd";
+  if (year % 2 == 0) {
+    value = "even";
+  } else {
+    value = "odd";
   }
 
-  for (final object in flybyObjects) {
-    print(object);
-  }
-
-  for (int month = 1; month <= 12; month++) {
+  for (int month = 1; month <= 2; month++) {
     print(month);
   }
 
-  while (year < 2022) {
-    year += 1;
-  }
-
-  print('The number is $year.'); // ? Print to console.
+  print('The number is $value : $year.'); // ? Print to console.
 }
 
 int fibonacci(int n) {
