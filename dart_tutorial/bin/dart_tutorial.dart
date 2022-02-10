baef9149185e7711b71ece1c786fafe94644432e
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-10 21:05:38 +0300

    added classes

--- dart_tutorial/bin/dart_tutorial.dart
+++ dart_tutorial/bin/dart_tutorial.dart
@@ -10,6 +10,10 @@ void main() {
   var number = 1998; // ? Declare and initialize a variable.
   printInteger(number); // ? Call a function.
   print(fibonacci(25));
+
+  // ? class call
+  var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+  voyager.describe();
 }
 
 void printInteger(int year) {
@@ -36,5 +40,35 @@ void arrowFunction() {
   var flybyObjects = ["sus", "susturn", "bruh"];
 
   // todo: A shorthand => (arrow) syntax is handy for functions that contain a single statement. This syntax is especially useful when passing anonymous functions as arguments:
+
   flybyObjects.where((name) => name.contains('turn')).forEach(print);
+
+  // ? Besides showing an anonymous function
+  // * (the argument to where())
+  // ? this code shows that you can use a function as an argument: the top-level print() function is an argument to forEach()
+}
+
+// todo: Here’s an example of a class with three properties, two constructors, and a method. One of the properties can’t be set directly, so it’s defined using a getter method (instead of a variable).
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  int? get launchYear => launchDate?.year;
+
+  Spacecraft(this.name, this.launchDate);
+
+  // todo: modern stype constructor
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion doesn't work on getters.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
 }
