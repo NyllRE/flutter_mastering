e81a9f6660758bdf0a8fcbf1e7b30104186d19ea
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-02-10 19:43:08 +0300

    added functions and beautiful comments

--- dart_tutorial/bin/dart_tutorial.dart
+++ dart_tutorial/bin/dart_tutorial.dart
@@ -1,3 +1,17 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+// void main(List<String> arguments) {
+//   print('Hello world!');
+// }
+
+// todo void : A special type that indicates a value that’s never used. Functions like printInteger() and main() that don’t explicitly return a value have the void return type
+
+void printInteger(int aNumber) {
+  print('The number is $aNumber.'); // ? Print to console.
+}
+
+// todo main(): The special, required, top-level function where app execution starts.
+// * This is where the app starts executing.
+
+void main() {
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
 }
