0172abc625b4e5d76dedec606b8aa23aff445281
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-09 00:01:51 +0300

    General Overview of All the Components

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  const RangeSelectorPage({Key? key}) : super(key: key);
+
+  /*
+  =>> This widget is the home page of your application. It is stateful, meaning that it has a State object (defined below) that contains fields that affect how it looks.
+  
+  =>> This class is the configuration for the state. It holds the values (in this case the title) provided by the parent (in this case the App widget) and used by the build method of the State. Fields in a Widget subclass are always marked "final".
+  */
+
+  @override
+  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Select Range'),
+        ),
+        body: Form(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const RangeSelectorField(labelText: 'Minimum'),
+                const SizedBox(height: 12),
+                RangeSelectorField(labelText: 'Maximum'),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.arrow_forward),
+          onPressed: () => {
+            //==<< Validate the form >>==//
+          },
+        ));
+  }
+}
+
+class RangeSelectorField extends StatelessWidget {
+  const RangeSelectorField({
+    Key? key,
+    required this.labelText,
+  }) : super(key: key);
+
+  final String labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'asdf',
+      ),
+      keyboardType: const TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+    );
+  }
+}
