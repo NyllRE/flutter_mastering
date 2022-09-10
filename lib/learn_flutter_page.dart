23e28098460f0dd89562dcb5c335adb638810c30
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-10 22:31:58 +0300

    Checkboxes & Image.network
    the app keeps crashing on certain stuff like if the checkbox is toggled or the FloatingActionButton is pressed, I really have no idea why it completely crashes and I'm starting to completely hate native development ._.

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -9,86 +9,99 @@ class LearnFlutterPage extends StatefulWidget {
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
   bool isSwitched = true;
+  bool? isCheckbox = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('deez driving me nuts'),
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.arrow_back_ios),
+        appBar: AppBar(
+          title: const Text('deez driving me nuts'),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
         ),
-      ),
 //
-      body: Column(
-        children: [
-          Image.asset('images/heisenburger.png'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(10),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                'deez',
-                style: TextStyle(color: Colors.white),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset('images/heisenburger.png'),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              primary: isSwitched ? Colors.green : Colors.blue,
-            ),
-            onPressed: () {
-              debugPrint('deez');
-            },
-            child: const Text('Elevated Button'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('deez');
-            },
-            child: const Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('deez');
-            },
-            child: const Text('Text Button'),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () => debugPrint('deez'),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(Icons.local_fire_department),
-                Text(
-                  'Row Widget',
+              const Divider(
+                color: Colors.black,
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.blueGrey,
+                width: double.infinity,
+                child: const Center(
+                  child: Text(
+                    'deez',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
-                Icon(Icons.local_fire_department, color: Colors.red),
-              ],
-            ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: isSwitched ? Colors.green : Colors.blue,
+                ),
+                onPressed: () {
+                  debugPrint('deez');
+                },
+                child: const Text('Elevated Button'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  debugPrint('deez');
+                },
+                child: const Text('Outlined Button'),
+              ),
+              TextButton(
+                onPressed: () {
+                  debugPrint('deez');
+                },
+                child: const Text('Text Button'),
+              ),
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => debugPrint('deez'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: const [
+                    Icon(Icons.local_fire_department),
+                    Text(
+                      'Row Widget',
+                    ),
+                    Icon(Icons.local_fire_department, color: Colors.red),
+                  ],
+                ),
+              ),
+              Switch(
+                value: isSwitched,
+                onChanged: (bool newVal) {
+                  setState(() {
+                    isSwitched = newVal;
+                  });
+                },
+              ),
+              
+              Checkbox(
+                  value: isCheckbox,
+                  onChanged: (bool? newBool) {
+                    setState(() {
+                      isCheckbox = newBool;
+                    });
+                  }),
+              Image.network(
+                  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjooinn.com%2Fimages%2Fgrayscale-mountain-photo-5.jpg')
+            ],
           ),
-          Switch(
-            value: isSwitched,
-            onChanged: (bool newVal) {
-              setState(() {
-                isSwitched = newVal;
-              });
-            },
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
