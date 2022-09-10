03241a5f4cdeea91fc25aa249e363d9d8bb7ef75
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-10 20:15:43 +0300

    Container props (margin, padding, sizing, positioning)

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -12,23 +12,36 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('deez driving me nuts'),
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(Icons.arrow_back_ios),
-          )),
+        title: const Text('deez driving me nuts'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+//
       body: Column(
         children: [
           Image.asset('images/heisenburger.png'),
-
           const SizedBox(
             height: 10,
           ),
           const Divider(
             color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            color: Colors.blueGrey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'deez',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           )
         ],
       ),
