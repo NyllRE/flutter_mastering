86025bdbd338bf7f68f72def0f9aeddadef8ef7e
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-19 18:49:41 +0300

    Using Arrays to Display Data

--- lib/udemy_course/udemy_course_app.dart
+++ lib/udemy_course/udemy_course_app.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mastering/udemy_course/answer.dart';
 import 'package:flutter_mastering/udemy_course/question.dart';
 
 class UdemyCourseApp extends StatelessWidget {
@@ -28,9 +29,18 @@ class RootPage extends StatefulWidget {
 class _RootPageState extends State<RootPage> {
   int qIdx = 0;
   List questions = [
-    'what is ball',
-    'how to ball',
-    'can you balling?',
+    {
+      'question': 'What is ball?',
+      'answers': ['deez', 'nuts'],
+    },
+    {
+      'question': 'how to ball?',
+      'answers': ['deez', 'nuts'],
+    },
+    {
+      'question': 'can you ball?',
+      'answers': ['deez', 'nuts'],
+    },
   ];
 
   void nextQ() {
@@ -52,8 +62,9 @@ class _RootPageState extends State<RootPage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Question(questions[qIdx]),
-            ElevatedButton(
+          Question(questions[qIdx]['question']),
+          Answer(questions[qIdx]['answers'][0], () => nextQ()),
+          /* ElevatedButton(
               child: Text(
                 'ball',
               ),
@@ -61,7 +72,7 @@ class _RootPageState extends State<RootPage> {
                 nextQ();
                 debugPrint('$qIdx');
               },
-            )
+            ) */
           ],
       ),
     );
