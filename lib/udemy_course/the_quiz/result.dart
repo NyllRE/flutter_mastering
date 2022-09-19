f92842fd96b379ae26710be117da792d2bae3fe1
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-19 20:45:02 +0300

    Conditional Result Message

--- lib/udemy_course/the_quiz/result.dart
+++ lib/udemy_course/the_quiz/result.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  final int? total;
+  final int total;
   const Result({Key? key, required this.total}) : super(key: key);
+
+  String get resMessage =>
+      total >= 8 ? 'Dayum! you got $total!' : 'nice you got $total';
 
   @override
   Widget build(BuildContext context) => Center(
         child: Text(
-          'You got $total',
+          resMessage,
           style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
         ),
       );
