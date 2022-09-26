805073048c38ad0f637bbeb7c773150f5c8dfe66
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-09-26 20:50:26 +0300

    separate transaction widget

---
+++ lib/expenses/widgets/transaction.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../reusable.dart';
+import '../models/transaction.dart';
+
+class TransactionWidget extends StatefulWidget {
+  final List<Transaction> transactions;
+  final Function removeTransaction;
+  final int idx;
+
+  const TransactionWidget(
+    this.transactions,
+    this.removeTransaction,
+    this.idx, {
+    super.key,
+  });
+
+  @override
+  State<TransactionWidget> createState() => _TransactionWidgetState();
+}
+
+class _TransactionWidgetState extends State<TransactionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: Edge(15, t: 10),
+            padding: Edge(15, t: 10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.purple,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              '\$${widget.transactions[widget.idx].amount}',
+              style: TextStyle(
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.transactions[widget.idx].title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                DateFormat('MM dd h:mm a')
+                    .format(widget.transactions[widget.idx].date),
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+          Container(
+            padding: Edge(15, t: 10),
+            child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    widget.removeTransaction(widget.idx);
+                  });
+                },
+                icon: Icon(Icons.delete)),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/expenses/widgets/transaction_list.dart
+++ lib/expenses/widgets/transaction_list.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
-import '/reusable.dart';
-import 'package:intl/intl.dart';
+import 'package:flutter_mastering/expenses/widgets/transaction.dart';
 import '../models/transaction.dart';
 
-class TransactionList extends StatefulWidget {
+class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function removeTransaction;
 
   TransactionList(this.transactions, this.removeTransaction);
 
   @override
-  State<TransactionList> createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  @override
   Widget build(BuildContext context) => Expanded(
       child: SizedBox(
           height: 500,
-      child: widget.transactions.isEmpty
+          child: transactions.isEmpty
           ? Center(
               child: Column(
                 children: const [
@@ -45,56 +39,12 @@ class _TransactionListState extends State<TransactionList> {
               ),
             )
           : ListView.builder(
-              itemCount: widget.transactions.length,
-              itemBuilder: (ctx, idx) => Card(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          margin: Edge(15, t: 10),
-                          padding: Edge(15, t: 10),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          child: Text(
-                            '\$${widget.transactions[idx].amount}',
-                            style: TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.transactions[idx].title,
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              DateFormat('MM dd h:mm a')
-                                  .format(widget.transactions[idx].date),
-                              style: TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                        Container(
-                          padding: Edge(15, t: 10),
-                          child: IconButton(
-                              onPressed: () {
-                                    setState(() {
-                                      widget.removeTransaction(idx);
-                                    });
-                              },
-                              icon: Icon(Icons.delete)),
-                        )
-                      ],
-                    ),
-                      ))));
+                  itemCount: transactions.length,
+                  itemBuilder: (_, idx) {
+                    return TransactionWidget(
+                        transactions, removeTransaction, idx);
+                  },
+                ),
+        ),
+      );
 }
